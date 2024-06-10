370ad93f200758bf1c78bc4c09eaf299774278f0
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-06-10 19:18:55 -0400

    add style and fuction pour top button

--- smoothscroll.js
+++ smoothscroll.js
@@ -10,6 +10,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 var mybutton = document.getElementById("backToTop");
 
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
@@ -23,6 +24,7 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 mybutton.onclick = function() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.querySelector('body').scrollIntoView({
+    behavior: 'smooth'
+  });
 }
