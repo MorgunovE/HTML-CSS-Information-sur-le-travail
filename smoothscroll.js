2df1c2dbf481b2d0aec8fb8fbde2c329e9ac686a
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-06-10 21:10:42 -0400

    add styles dnas tableu

--- smoothscroll.js
+++ smoothscroll.js
@@ -8,7 +8,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-var mybutton = document.getElementById("backToTop");
+let backToTop = document.getElementById("backToTop");
 
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -16,14 +16,14 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    backToTop.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    backToTop.style.display = "none";
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-mybutton.onclick = function() {
+backToTop.onclick = function() {
   document.querySelector('body').scrollIntoView({
     behavior: 'smooth'
   });
